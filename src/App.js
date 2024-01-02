7321b64f5fc0446ed1f18b9f268837a09b6462b5
Dominik Loncar <dominikloncar@outlook.com>
2024-01-02 16:06:59 +0100

    Added inline style to component, passed it properly.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import { useState } from 'react';
 import './App.css';
 
-function SomeText({p1, p2}) {
+function SomeText({p1, p2="p2", style={}}) {
   return (
-    <div>
+    <div style={style}>
       <p>{p1}</p>
       <p>{p2}</p>
     </div>
@@ -18,6 +18,9 @@ function App() {
     <div className="App">
       <p>Paragraph before.</p>    
       <p onClick={() => setP1(p1+1)}> {`Clicked ${p1} times.`} </p>  
+      <p onClick={() => setP1(p1+1)}> {`Clicked ${p1} times.`} </p>  
+      <p onClick={() => setP1(p1+1)}> {`Clicked ${p1} times.`} </p>  
+      <SomeText p1={p1} style={{backgroundColor: "black", color: "white", paddingTop: "20px", paddingBottom: "20px"}} />
     </div>
   );
 }

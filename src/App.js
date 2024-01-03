2a9fb9e8b96bc718c343ce50d3071207de93960b
Dominik Loncar <dominikloncar@outlook.com>
2024-01-03 15:04:41 +0100

    Removed unneeded code.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import { useState, Suspense, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 
-function SomeText({p1, p2="p2", style={}}) {
+function SomeText({p1, style={}}) {
   
   return (
     <div style={style}>
       <p>Double it! {p1}</p>
-      <p>{p2}</p>
     </div>
     )
 }
@@ -20,18 +19,9 @@ function App() {
     <div className="App">
       <p>Paragraph before.</p>    
       <p onClick={() => setP1(p1+1)}> {`Clicked ${p1} times.`} </p>  
-      <p onClick={() => setP1(p1+1)}> {`Clicked ${p1} times.`} </p>  
-      <p onClick={() => setP1(p1+1)}> {`Clicked ${p1} times.`} </p>  
-
-      <Suspense fallback={<Loading />}>
-        <SomeText p1={p1} style={{backgroundColor: "black", color: "white", paddingTop: "20px", paddingBottom: "20px"}} />
-      </Suspense>
+      <SomeText p1={p1} style={{backgroundColor: "black", color: "white", paddingTop: "20px", paddingBottom: "20px"}} />
     </div>
   );
-}
-
-const Loading = () => {
-    return <div>Loading...</div>
 }
 
 export default App;

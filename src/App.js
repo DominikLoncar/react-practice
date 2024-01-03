927c07c462b14c59f2eb96588ed84448e350d4f8
Dominik Loncar <dominikloncar@outlook.com>
2024-01-03 15:04:03 +0100

    Suspense practice.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import { useState } from 'react';
+import { useState, Suspense, useEffect } from 'react';
 import './App.css';
 
+
 function SomeText({p1, p2="p2", style={}}) {
+  
   return (
     <div style={style}>
-      <p>{p1}</p>
+      <p>Double it! {p1}</p>
       <p>{p2}</p>
     </div>
     )
@@ -20,9 +22,16 @@ function App() {
       <p onClick={() => setP1(p1+1)}> {`Clicked ${p1} times.`} </p>  
       <p onClick={() => setP1(p1+1)}> {`Clicked ${p1} times.`} </p>  
       <p onClick={() => setP1(p1+1)}> {`Clicked ${p1} times.`} </p>  
-      <SomeText p1={p1} style={{backgroundColor: "black", color: "white", paddingTop: "20px", paddingBottom: "20px"}} />
+
+      <Suspense fallback={<Loading />}>
+        <SomeText p1={p1} style={{backgroundColor: "black", color: "white", paddingTop: "20px", paddingBottom: "20px"}} />
+      </Suspense>
     </div>
   );
+}
+
+const Loading = () => {
+    return <div>Loading...</div>
 }
 
 export default App;
